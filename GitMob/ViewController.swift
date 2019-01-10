002305dacbe7c2136586dab11ac9f2a364375bc2
hineso11 <oliver.hines@btinternet.com>
2019-01-10 11:25:25 +0000

    Initial Commit

---
+++ GitMob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitMob
+//
+//  Created by Oliver Hines on 10/01/2019.
+//  Copyright © 2019 Oliver Hines Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
